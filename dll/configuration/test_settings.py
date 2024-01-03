ca62bd0955e1768b1118cad211b50d4fb7719286
Robert Stein <robert@blueshoe.de>
2024-01-03 09:55:05 +0100

    chore: remove terms middleware for tests

--- dll/configuration/test_settings.py
+++ dll/configuration/test_settings.py
@@ -2,4 +2,7 @@ from dll.configuration.settings import *
 
 # remove 'haystack' from INSTALLED_APPS
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != "haystack"]
-print("INSTALLED_APPS", INSTALLED_APPS)
+
+MIDDLEWARE = [
+    m for m in MIDDLEWARE if m != "dll.user.middleware.AcceptedTermsMiddleware"
+]
